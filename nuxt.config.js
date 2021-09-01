46d69d8417f5af6fe41bf6cff830a3502173fd35
Art <pramot5525@gmail.com>
2021-09-02 00:48:46 +0700

    Update template

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,8 @@
 export default {
+  target: 'static',
+  router: {
+    base: '/art-portfolio/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'art-portfolio',
